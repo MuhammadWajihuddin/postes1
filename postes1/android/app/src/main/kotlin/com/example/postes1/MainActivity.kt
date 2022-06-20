dd5f876594c91b039e999e65d7e34cd9cd7430aa
MuhammadWajihuddin <mwajihuddin1412@gmail.com>
2022-06-20 17:45:52 +0800

    postes1

---
+++ postes1/android/app/src/main/kotlin/com/example/postes1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.postes1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ postes1/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        home: MyHomePage(
+      title: "POSTES 1",
+    ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: hexStringToColor("363062"),
+        title: const Text(
+          "PosTes1  1915016106",
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("BE8ABF"),
+          hexStringToColor("827397"),
+          hexStringToColor("4D4C7D")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+      ),
+    );
+  }
+}
+
+hexStringToColor(String hexColor) {
+  hexColor = hexColor.toUpperCase().replaceAll("#", "");
+  if (hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
+  }
+  return Color(int.parse(hexColor, radix: 16));
+}
